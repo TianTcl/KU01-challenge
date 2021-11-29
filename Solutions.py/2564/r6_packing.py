f06de28cfeb69679efc13bb2c5b89f969215eb6d
TianTcl <49802993+TianTcl@users.noreply.github.com>
2021-11-29 14:35:44 +0700

    Optimize packing

--- Solutions.py/2564/r6_packing.py
+++ Solutions.py/2564/r6_packing.py
@@ -2,8 +2,7 @@
 # ifo_size = max({ifo_maxX//each_size for each_size in range(ifo_minX, ifo_minY + 1)}.union({ifo_maxY//each_size for each_size in range(ifo_minX, ifo_minY + 1)}))
 # amt_used = ifo_size * (ifo_maxX if "check side" else ifo_maxY)
 
-[ifo_maxX, ifo_maxY, ifo_minX, ifo_minY], amt_left = [int(a) for a in input().split()], float("inf")
-for each_size in range(ifo_minX, ifo_minY + 1): amt_left = min(amt_left, (ifo_maxX % each_size) * (ifo_maxY % each_size))
-print(amt_left)
+[ifo_maxX, ifo_maxY, ifo_minX, ifo_minY] = [int(a) for a in input().split()]
+print(min([(ifo_maxX % each_size) * (ifo_maxY % each_size) for each_size in range(ifo_minX, ifo_minY + 1)]))
 # Passed
 # 100%
