3e91b580683f7a9762eae0f232c62ff094279ca6
TianTcl <chainathlim@gmail.com>
2023-12-07 17:44:04 +0700

    Change score

--- Solutions.py/2563/re_stairs.py
+++ Solutions.py/2563/re_stairs.py
@@ -16,7 +16,7 @@
 # for each_room in range(amt_room): ifo_stair.extend([c for c in range(ifo_room[each_room]+(1 if ifo_room[each_room]<ifo_room[each_room+1] else -1), ifo_room[each_room+1], (1 if ifo_room[each_room]<ifo_room[each_room+1] else -1))])
 # for each_range in range(amt_range): print(len(list(filter(lambda d: d >= ifo_range[each_range][0] and d <= ifo_range[each_range][1] and d % 2 == 0, ifo_stair))), len(list(filter(lambda d: d >= ifo_range[each_range][0] and d <= ifo_range[each_range][1] and d % 2 == 1, ifo_stair))))
 # Passed
-# 10% [[PPP][xxxx][TTT][xxxxxxx]]
+# 25% [[PPP][xxxx][TTT][xxxxxxx]]
 
 amt_room, ifo_room, amt_range, ifo_stair = int(input()), [int(a) for a in ("0 "+input()).split()], int(input()), list(())
 for each_room in range(amt_room): ifo_stair.extend([c for c in range(ifo_room[each_room]+(1 if ifo_room[each_room]<ifo_room[each_room+1] else -1), ifo_room[each_room+1], (1 if ifo_room[each_room]<ifo_room[each_room+1] else -1))])
@@ -24,4 +24,4 @@ for each_range in range(amt_range):
     ifo_range = [int(b) for b in input().split()]
     print(len(list(filter(lambda d: d >= ifo_range[0] and d <= ifo_range[1] and d % 2 == 0, ifo_stair))), len(list(filter(lambda d: d >= ifo_range[0] and d <= ifo_range[1] and d % 2 == 1, ifo_stair))))
 # Passed
-# 10% [[PPP][xxxx][TTT][xxxxxxx]]
+# 25% [[PPP][xxxx][TTT][xxxxxxx]]
